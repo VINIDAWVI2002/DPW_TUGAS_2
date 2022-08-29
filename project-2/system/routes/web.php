a3e08f53a56921ea19f1bcb89aadedf0299374a9
VINIDAWVI2002 <vinidawvi@gmail.com>
2022-08-29 13:19:58 +0700

    TUGAS 2

---
+++ project-2/system/routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+
+Route::get('cart', function () {
+    return view('cart');
+});
+
+
+Route::get('category', function () {
+    return view('category');
+});
+
+Route::get('contact_process', function () {
+    return view('contact_process');
+});
+
+
+Route::get('checkout', function () {
+    return view('checkout');
+});
+
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('single-product', function () {
+    return view('single-product');
+});
